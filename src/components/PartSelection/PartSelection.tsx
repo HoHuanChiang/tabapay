e655066754349ceb000d9dfe02510af629603baf
Ho-Huan Chiang <hc57@rice.edu>
2023-05-25 12:51:05 -0400

    update base url path

--- src/components/PartSelection/PartSelection.tsx
+++ src/components/PartSelection/PartSelection.tsx
@@ -1,3 +1,4 @@
+import { Path } from "../../App";
 import {
     StyledLinkContainer,
     StyledLinkGroupContainer,
@@ -11,22 +12,22 @@ const PartSelection = () => {
         <StyledOuterContainer>
             <StyledTitle>Take Home Project - TabaPay</StyledTitle>
             <StyledLinkGroupContainer>
-                <Link to="/part1">
+                <Link to={Path.Part1}>
                     <StyledLinkContainer>Part 1</StyledLinkContainer>
                 </Link>
-                <Link to="/part2">
+                <Link to={Path.Part2}>
                     <StyledLinkContainer>Part 2</StyledLinkContainer>
                 </Link>
-                <Link to="/part3">
+                <Link to={Path.Part3}>
                     <StyledLinkContainer>Part 3</StyledLinkContainer>
                 </Link>
-                <Link to="/part4">
+                <Link to={Path.Part4}>
                     <StyledLinkContainer>Part 4</StyledLinkContainer>
                 </Link>
-                <Link to="/part5">
+                <Link to={Path.Part5}>
                     <StyledLinkContainer>Part 5</StyledLinkContainer>
                 </Link>
-                <Link to="/part6">
+                <Link to={Path.Part6}>
                     <StyledLinkContainer>Part 6</StyledLinkContainer>
                 </Link>
             </StyledLinkGroupContainer>
